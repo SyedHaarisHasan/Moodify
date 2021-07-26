caf11ec162127216cac08ecb26fee5754f92672e
Syed <haaris.hasan03@gmail.com>
2021-07-26 19:29:16 -0400

    updated Track constructor

--- Track.cpp
+++ Track.cpp
@@ -1,6 +1,6 @@
 #include "Track.h"
 
-Track::Track(string _name, string _artist, int _popularity, int _xplicit, int _releaseYear, int _danceability, int _energy, int _valence) {
+Track::Track(string _name, string _artist, int _popularity, int _xplicit, int _releaseYear, float _danceability, float _energy, float _valence) {
     name = _name;
     artist = _artist;
     popularity = _popularity;
--- Track.h
+++ Track.h
@@ -10,7 +10,7 @@ private:
     float danceability, energy, valence;
 public:
     //constructor
-    Track(string _name, string _artist, int _popularity, int _xplicit, int _releaseYear, int _danceability, int _energy, int _valence);
+    Track(string _name, string _artist, int _popularity, int _xplicit, int _releaseYear, float _danceability, float _energy, float _valence);
 
     //accessors
     string getName();
