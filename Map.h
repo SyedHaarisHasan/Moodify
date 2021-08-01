48a18ab7f8aa82c9aff2edcfe1b20939441d6f1f
rebeccajhampton <73967689+rebeccajhampton@users.noreply.github.com>
2021-08-01 13:57:09 -0400

    Updated map.h

--- Map.h
+++ Map.h
@@ -31,4 +31,12 @@ public:
 	Node* root;
 	void insert(Track track, Node*& root);
 	void printInorder(Node* root, vector<Track>& v);
+	void findSongName(Node* root, vector<Track>& playlist, string song);
+	void findSongArtist(Node* root, vector<Track>& playlist, string artist);
+	void findSongPopular(Node* root, vector<Track>& playlist, bool isPopular);
+	void findSongExplicit(Node* root, vector<Track>& playlist, bool isExplicit);
+	void findSongDate(Node* root, vector<Track>& playlist, int bY, int eY);
+	void findSongDance(Node* root, vector<Track>& playlist, char c);
+	void findSongEnergy(Node* root, vector<Track>& playlist, char c);
+	void findSongValence(Node* root, vector<Track>& playlist, char c);
 };
