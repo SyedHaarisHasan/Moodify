07179b447400a3d2fa8366e0504fd3123d8a58e5
rebeccajhampton <73967689+rebeccajhampton@users.noreply.github.com>
2021-08-01 13:43:52 -0400

    Updated main with completed map functionality

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,242 +1,287 @@
 #include "HashTable.h"
-#include <random>
-#include <ctime>
+#include "Track.h"
+#include "Map.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
-extern const int SIZE = 580419;
 
-int HashTable::hash(string key) { // hash algorithm
-    int index = 0;
+void loadHashTable(HashTable& table);
+void loadMap(Map& map);
 
-    for (int i = 1; i <= key.size(); i++)
-        index += key[i - 1] * i;
+int main() {
+    cout << "Welcome to Moodify!\nPlease answer the following questions.\n\n"; // menu TO-FIX
+    int ds = 0;
 
-    index = index % 580471;
+    while (true) { // loops until user inputs valid number    
+        if (ds != 1 && ds != 2) {
+            cout << "Which data structure would you like to use?\n";
+            cout << "1. Map\n2. Hash Table\n";
+            cin >> ds;
+            cout << endl;
+        }
+        else
+            break;
+    }
 
-    if (index < 0) { // if index is invalid, add track to 0th index
-        return 580418;
+    if (ds == 1) { // if data structure is map
+        Map map;
+        loadMap(map);
+
+        vector<Track> playlist;
+        cout << "Is there a specific song you want to listen to? " << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        char c;
+        cin >> c;
+        if (c == 'A') {
+            cout << "What do you want to listen to? " << endl;
+            string song;
+            cin >> song;
+            map.findSongName(map.root, playlist, song);
+        }
+
+        cout << "Do you want to listen to a popular song?" << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            map.findSongPopular(map.root, playlist, true);
+        }
+        else {
+            map.findSongPopular(map.root, playlist, false);
+        }
+
+        cout << "Are you ok if the song is explicit?" << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            map.findSongExplicit(map.root, playlist, true);
+        }
+        else if (c == 'B') {
+            map.findSongExplicit(map.root, playlist, false);
+        }
+
+        cout << "Is there an artist you want to listen to? " << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            cout << "Who do you want to listen to? " << endl;
+            string name;
+            cin >> name;
+            map.findSongArtist(map.root, playlist, name);
+        }
+
+        cout << "How old do you want your song to be" << endl;
+        cout << "A: Recent" << endl;
+        cout << "B: Old" << endl;
+        cout << "C: Ancient" << endl;
+        cout << "D: Doesn't matter" << endl;
+        cout << "E: Custom year" << endl;
+        cin >> c;
+        switch (c) {
+        case 'A':
+            map.findSongDate(map.root, playlist, 2016, 2021);
+            break;
+        case 'B':
+            map.findSongDate(map.root, playlist, 2000, 2016);
+            break;
+        case 'C':
+            map.findSongDate(map.root, playlist, 1980, 2000);
+            break;
+        case 'E':
+            cout << "What year?" << endl;
+            string isYear;
+            cin >> isYear;
+            map.findSongDate(map.root, playlist, stoi(isYear), stoi(isYear));
+            break;
+        }
+
+        cout << "How danceable do you want your song to be?" << endl;
+        cout << "A: not danceable" << endl;
+        cout << "B: somewhat danceable" << endl;
+        cout << "C: danceable" << endl;
+        cin >> c;
+        map.findSongDance(map.root, playlist, c);
+
+        cout << "How much energy do you want your song to be?" << endl;
+        cout << "A: not much energy" << endl;
+        cout << "B: somewhat energy" << endl;
+        cout << "C: a lot of energy" << endl;
+        cin >> c;
+
+        map.findSongValence(map.root, playlist, c);
+        cout << "How valence do you want your song to be?" << endl;
+        cout << "A: not much valence" << endl;
+        cout << "B: somewhat valent" << endl;
+        cout << "C: a lot of valence" << endl;
+        cin >> c;
+        map.findSongEnergy(map.root, playlist, c);
+
+        cout << "Ok, here are 5 songs that we think you might like" << endl;
+        for (int i = 0; i < 5; i++) {
+            cout << playlist[i].getName() << ",";
+            cout << playlist[i].getArtist() << ",";
+            cout << playlist[i].getPopularity() << ",";
+            cout << playlist[i].getExplicit() << ",";
+            cout << playlist[i].getReleaseYear() << ",";
+            cout << playlist[i].getDanceability() << ",";
+            cout << playlist[i].getEnergy() << ",";
+            cout << playlist[i].getValence() << endl;
+        }
     }
-    else
-        return index;
+    else { // if data structure is hash table
+        HashTable table;
+        loadHashTable(table);
+        vector<Track> playlist;
+        cout << "Is there a specific song you want to listen to? " << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        char c;
+        cin >> c;
+        if (c == 'A') {
+            cout << "What do you want to listen to? " << endl;
+            string song;
+            cin >> song;
+            playlist = table.findSongName(playlist, song);
+        }
+        cout << "Do you want to listen to a popular song?" << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            playlist = table.findSongPopular(playlist, true);
+        }
+        else {
+            playlist = table.findSongPopular(playlist, false);
+        }
+        cout << "Are you ok if the song is explicit?" << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            playlist = table.findSongExplicit(playlist, true);
+        }
+        else if (c == 'B') {
+            playlist = table.findSongExplicit(playlist, false);
+        }
+        cout << "Is there an artist you want to listen to? " << endl;
+        cout << "A: Yes" << endl;
+        cout << "B: No" << endl;
+        cin >> c;
+        if (c == 'A') {
+            cout << "Who do you want to listen to? " << endl;
+            string name;
+            cin >> name;
+            playlist = table.findSongArtist(playlist, name);
+        }
+        cout << "How old do you want your song to be" << endl;
+        cout << "A: Recent" << endl;
+        cout << "B: Old" << endl;
+        cout << "C: Ancient" << endl;
+        cout << "D: Doesn't matter" << endl;
+        cout << "E: Custom year" << endl;
+        cin >> c;
+        switch (c) {
+        case 'A':
+            playlist = table.findSongDate(playlist, 2016, 2021);
+            break;
+        case 'B':
+            playlist = table.findSongDate(playlist, 2000, 2016);
+            break;
+        case 'C':
+            playlist = table.findSongDate(playlist, 1980, 2000);
+            break;
+        case 'E':
+            cout << "What year?" << endl;
+            string isYear;
+            cin >> isYear;
+            playlist = table.findSongDate(playlist, stoi(isYear), stoi(isYear));
+            break;
+        }
+        cout << "How danceable do you want your song to be?" << endl;
+        cout << "A: not danceable" << endl;
+        cout << "B: somewhat danceable" << endl;
+        cout << "C: danceable" << endl;
+        cin >> c;
+        playlist = table.findSongDance(playlist, c);
+        cout << "How much energy do you want your song to be?" << endl;
+        cout << "A: not much energy" << endl;
+        cout << "B: somewhat energy" << endl;
+        cout << "C: a lot of energy" << endl;
+        cin >> c;
+        playlist = table.findSongValence(playlist, c);
+        cout << "How valence do you want your song to be?" << endl;
+        cout << "A: not much valence" << endl;
+        cout << "B: somewhat valent" << endl;
+        cout << "C: a lot of valence" << endl;
+        cin >> c;
+        playlist = table.findSongEnergy(playlist, c);
+        cout << "Ok, here are 5 songs that we think you might like" << endl;
+        table.Print(playlist);
+    }
+
+    return 0;
 }
 
-void HashTable::insert(Track track) { // inserts 'track' at hashed value of 'track's' name
-    int index = hash(track.getName());
-    hashTable[index].push_back(track);
+void loadHashTable(HashTable& table) {
+    ifstream file("tracks.csv");
+    string name, artist, popularity, xplicit, releaseYear, danceability, energy, valence;
+
+    cout << "Loading..." << endl;
+
+    getline(file, name); // clear header
+    while (!file.eof()) { // File I/O
+        getline(file, name, ',');
+        getline(file, popularity, ',');
+        getline(file, xplicit, ',');
+        getline(file, artist, ',');
+        getline(file, releaseYear, ',');
+        getline(file, danceability, ',');
+        getline(file, energy, ',');
+        getline(file, valence, '\n');
+
+        for (auto& i : name) { // handle dollar signs in 'name'
+            if (i == '$')
+                i = ',';
+        }
+
+        if (releaseYear.size() != 4) // update 'releaseYear' if needed
+            releaseYear = releaseYear.substr(releaseYear.size() - 4, 4);
+
+        Track track(name, artist, stoi(popularity), stoi(xplicit), stoi(releaseYear), stof(danceability), stof(energy), stof(valence));
+        table.insert(track);
+    }
 }
 
-int HashTable::getSize() {
-    return SIZE;
-}
+void loadMap(Map& map) {
+    ifstream file("data/tracks.csv");
+    string name, artist, artistList, popularity, xplicit, releaseYear, danceability, energy, valence;
 
-list<Track> HashTable::operator[](int index) { // returns list of Tracks contained at 'index'
-    return hashTable[index];
-}
+    cout << "Loading..." << endl;
 
-void HashTable::printTable() { // prints the table // FOR DEBUGGING PURPOSES
-    for (int i = 0; i < SIZE; i++) {
-        for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-            cout << it->getName() << endl;
+    getline(file, name); // clear header
+    while (!file.eof()) { // File I/O
+        getline(file, name, ',');
+        getline(file, popularity, ',');
+        getline(file, xplicit, ',');
+        getline(file, artist, ',');
+        getline(file, artistList, ',');
+        getline(file, releaseYear, ',');
+        getline(file, danceability, ',');
+        getline(file, energy, ',');
+        getline(file, valence, '\n');
+
+        for (auto& i : name) { // handle dollar signs in 'name'
+            if (i == '$')
+                i = ',';
         }
-    }
-}
-vector<Track> HashTable::findSongName(vector<Track>& playlist, string song)
-{
-    for (int i = 0; i < SIZE; i++) {
-        for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-            if (it->getName() == song) {
-                playlist.push_back(*it);
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongArtist(vector<Track>& playlist, string artist)
-{
-    for (int i = 0; i < SIZE; i++) {
-        for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-            if (it->getArtist() == artist) {
-                playlist.push_back(*it);
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongPopular(vector<Track>& playlist, bool isPopular)
-{
-    if (isPopular) {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getPopularity() != 0) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getPopularity() == 0) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongExplicit(vector<Track>& playlist, bool isExplicit)
-{
-    if (isExplicit) {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getExplicit() != 0) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getExplicit() != 1) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongDate(vector<Track>& playlist, int bY, int eY)
-{
-    int difference = eY - bY;
-    if (difference == 0) {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getReleaseYear() == eY) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getReleaseYear() >= bY && it->getReleaseYear() <= eY) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongDance(vector<Track>& playlist, char c)
-{
-    if (c == 'A') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getDanceability() > 0 && it->getDanceability() < .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'B') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getDanceability() < 0.67 && it->getDanceability() > .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'C') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getDanceability() > 0.67) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongEnergy(vector<Track>& playlist, char c)
-{
-    if (c == 'A') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getEnergy() > 0 && it->getEnergy() < .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'B') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getEnergy() < 0.67 && it->getEnergy() > .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'C') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getEnergy() > 0.67) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-vector<Track> HashTable::findSongValence(vector<Track>& playlist, char c)
-{
-    if (c == 'A') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getValence() > 0 && it->getValence() < .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'B') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getValence() < 0.67 && it->getValence() > .33) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    else if (c == 'C') {
-        for (int i = 0; i < SIZE; i++) {
-            for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-                if (it->getValence() > 0.67) {
-                    playlist.push_back(*it);
-                }
-            }
-        }
-    }
-    return playlist;
-}
-void HashTable::Print(vector<Track>& playlist)
-{
-    int finish = 0;
-    for (int i = 0; i < SIZE; i++) {
-        for (auto it = hashTable[i].begin(); it != hashTable[i].end(); it++) {
-            if (finish == 5) {
-                exit;
-            }
-            if (it->getName() == "[]") {
-                continue;
-            }
-            else {
-                cout << it->getName() << " by " << it->getArtist() << endl;
-                finish++;
-            }
-        }
+
+        if (releaseYear.size() != 4) // update 'releaseYear' if needed
+            releaseYear = releaseYear.substr(releaseYear.size() - 4, 4);
+
+        Track track(name, artist, stoi(popularity), stoi(xplicit), stoi(releaseYear), stof(danceability), stof(energy), stof(valence));
+        map.insert(track, map.root);
     }
 }
